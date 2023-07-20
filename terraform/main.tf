4d577f7ec9a1e4b93ff090ae7ba903bca0a7d8dc
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2023-07-20 12:07:30 +0200

    Create aws_vpc resource - install terraform fmt on ubuntu

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "~> 4.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
-    backend "s3" {}
+  }
+  backend "s3" {}
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
