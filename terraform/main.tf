ae25bd68b508710e9cb9795d6fee9f6c64a0eb08
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2023-07-20 07:42:33 +0200

    First commit with variables specified

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
       }
     }
     backend "s3" {
-      bucket = "example-bucket"
+      bucket = "githubmax-oidc-terraformstate"
       key = "terraform.tfstate"
       region = "eu-central-1"
     }
